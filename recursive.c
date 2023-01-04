0ff84ca20ecbb23d0bb175104610314b68930f23
AMENAB12 <lalatube71@gmail.com>
2023-01-04 07:36:46 -0800

    c

--- recursive.c
+++ recursive.c
@@ -1,27 +1,27 @@
 /**Sum of all natural numbers*/
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// int sum(int n);
+int sum(int n);
 
-// int main(){
-// int number, result;
-// printf("please Enter a positive number only : ");
-// scanf("%d", &number);
+int main(){
+int number, result;
+printf("please Enter a positive number only : ");
+scanf("%d", &number);
 
-// result = sum(number);
+result = sum(number);
 
-// printf("Sum = %d", result);
-//     return  0;
-// }
-// int sum(int n){
-//     //function sum is calling it self
-//     if (n != 0){
-//       return n + sum(n - 1);  
-//     } else {
-//         return n;
-//     }
-// }
+printf("Sum = %d", result);
+    return  0;
+}
+int sum(int n){
+    //function sum is calling it self
+    if (n != 0){
+      return n + sum(n - 1);  
+    } else {
+        return n;
+    }
+}
 /** Fibonacci Series*/
 #include <stdio.h>
 
