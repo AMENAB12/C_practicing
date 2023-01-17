f3a0a6e859d179e28e39caad53e04871d8ff7dbd
AMENAB12 <lalatube71@gmail.com>
2023-01-16 16:15:21 -0800

    new change

--- while.c
+++ while.c
@@ -10,3 +10,35 @@ int main(void)
 	} while (i < 1);
 		return 0;
 }
+/** While loop*/
+#include <stdio.h>
+
+int main(){
+int i = 0;
+ 
+while ( i <= 100){
+   if (i % 3 == 0){
+       printf("fizz", i);
+   } else if (i % 5 == 0){
+       printf("buzz", i);
+   }
+   
+    printf("%d\n", i);
+    i = i + 5;
+}
+return 0;
+}
+/** Do while loop*/
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 0;
+	do
+	{
+		printf("number %d\n", i);
+		i++;
+	} while (i <= 100);
+		return 0;
+}
+/** for loop*/
